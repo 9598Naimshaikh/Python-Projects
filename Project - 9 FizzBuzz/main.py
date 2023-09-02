b4d85fb6b51bab355e8190c202c50a448fc60ae0
9598Naimshaikh <nn9412466@gmail.com>
2023-09-02 18:27:28 +0530

    Update main.py

--- Project - 9 FizzBuzz/main.py
+++ Project - 9 FizzBuzz/main.py
@@ -5,19 +5,19 @@
 
 
 for i in range(1, 100):
-    if (i==3):
-        print('Fizz')
+    if i == 3:
+        print("Fizz")
         continue
-    elif (i%5 == 0 and i%3 == 0):
-        print('FizzBuzz')
+    elif i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
         continue
-    elif (i==5):
+    elif i == 5:
         print("Buzz")
         continue
-    elif (i%3 == 0):
-        print('Fizz')
+    elif i % 3 == 0:
+        print("Fizz")
         continue
-    elif (i%5 == 0):
-        print('Buzz')
+    elif i % 5 == 0:
+        print("Buzz")
         continue
     print(i)
