e8638d27cec3c8387eafe1f640557b2a997d1d95
9598Naimshaikh <nn9412466@gmail.com>
2023-09-05 12:10:16 +0530

    Update main.py

--- Project - 11 Fibonacci Series/main.py
+++ Project - 11 Fibonacci Series/main.py
@@ -22,3 +22,44 @@ def fibonacci_series(num):
 if __name__ == "__main__":
     num = int(input("Enter a num: "))
     fibonacci_series(num)
+
+
+
+## 6. Create a Fibonacci Series By using Recursion..
+"""
+def fib_recursion(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fib_recursion(num-1) + fib_recursion(num-2)
+    
+
+num = int(input('Enter a num: '))
+print(fib_recursion(num))
+
+# #if you want to input to number of terms then do this..
+# num = int(input('which number of term do you want to genrate fibonacci: '))
+# for n in range(num):
+#     print(fib_recursion(n))
+"""
+
+
+## 1. Genrate an infinite fibonacci series by using Generator..
+"""
+def fibonacci_genrator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a+b
+
+f1 = fibonacci_genrator()
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+"""
