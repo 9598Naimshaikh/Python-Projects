4b675563a10f3866d8720d4fc4cc5f7c8447c2b3
9598Naimshaikh <nn9412466@gmail.com>
2023-09-05 16:41:51 +0530

    Update main.py

--- Project - 16 Palindrome Checker/main.py
+++ Project - 16 Palindrome Checker/main.py
@@ -1,18 +1,27 @@
-## Question: Palindrome Checker ?
+# Problem: Palindrome Checker
+
+# Write a Python function that checks whether a given string is a palindrome.
+# A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+import string
+
 
 def palindrome_checker(word):
-    word = word.lower()
-    reverse_word = word[::-1]
+    word = word.lower()  # convert to all lower case.
+    remove_punc = word.translate(str.maketrans("", "", string.punctuation)).split() #remove punctuation and convert to string to remove space.
 
-    print(f'Word: {word}')
-    print(f'Reverse Word: {reverse_word}')
-    
-    if word == reverse_word:
-        print('Yes.! Palindrome.')
+    modify_word = "".join(remove_punc)
+    reverse_word = modify_word[::-1]  # reverse the string.
+
+    print(f"Modify_word: {modify_word}")
+    print(f"Reverse Word: {reverse_word}")
+
+    if modify_word == reverse_word:
+        print("Yes.! Palindrome.")
     else:
-        print('Not.! Palindrome.')
+        print("Not.! Palindrome.")
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     word = input('Enter a word: ')
     palindrome_checker(word)
