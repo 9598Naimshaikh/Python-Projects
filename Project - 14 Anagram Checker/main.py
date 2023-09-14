f86486436a0635ceb15737f3e8bd2d120e0b37e4
9598Naimshaikh <nn9412466@gmail.com>
2023-09-14 16:37:24 +0530

    Update main.py

--- Project - 14 Anagram Checker/main.py
+++ Project - 14 Anagram Checker/main.py
@@ -1,21 +1,25 @@
 # Question: Anagram Checker
-# Write a Python program that takes two strings as input and determines whether they are anagrams of each other. An anagram is a word or phrase formed by rearranging the letters of another, using all the original letters exactly once.
+
+# Write a Python program that takes two strings as input and determines whether they are anagrams of each other.
+# An anagram is a word or phrase formed by rearranging the letters of another,
+# using all the original letters exactly once.
 
 
-def Anagrams(str1, str2):
-    str1 = "".join(sorted(f.lower()))
-    str2 = "".join(sorted(s.lower()))
+def Anagrams(word1, word2):
+    word1 = "".join(sorted(word1.lower()))
+    word2 = "".join(sorted(word2.lower()))
 
-    print(str1)
-    print(str2)
+    print(word1)
+    print(word2)
 
-    if str1 == str2:
-        print("Anagrams")
+    if word1 == word2:
+        print("Yes.! Anagrams")
     else:
-        print("Not Anagrams")
+        print("Not.! Anagrams")
 
 
 if __name__ == "__main__":
-    f = input("enter a first anagram string: ")
-    s = input("enter a second anagram string: ")
-    Anagrams(f, s)
+    str1 = input("enter a first anagram string: ")
+    str2 = input("enter a second anagram string: ")
+
+    Anagrams(str1, str2)
